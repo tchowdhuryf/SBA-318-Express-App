3383a72064a8f6d5db28e6bda2f79ac7c09ae55c
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2025-01-11 02:52:33 -0500

    trivia answers turn red or green when correct or incorrect

--- js/script.js
+++ js/script.js
@@ -1,32 +1,39 @@
 async function startGame(category) {
-    const response = await fetch(`api/questions/${category}`);
-    const question = await response.json();
-    console.log(response);
-    displayQuestion(question);
-  }
-  
-  function displayQuestion(question) {
-    document.getElementById('game-container').style.display = 'none';
-    const questionContainer = document.getElementById('question-container');
-    questionContainer.style.display = 'block';
-  
-    document.getElementById('question').innerText = question.question;
-    const options = document.getElementById('options');
-    options.innerHTML = '';
-  
-    question.options.forEach(option => {
-      const li = document.createElement('li');
-      li.innerText = option;
-      li.onclick = () => checkAnswer(option, question.answer);
-      options.appendChild(li);
-    });
-  }
-  
-  function checkAnswer(selected, correct) {
-    if (selected === correct) {
-      alert('Correct!');
-    } else {
-      alert('Wrong!');
+  const response = await fetch(`api/questions/${category}`);
+  const question = await response.json();
+  console.log(response);
+  displayQuestion(question);
+}
+
+function displayQuestion(question) {
+  document.getElementById("game-container").style.display = "none";
+  const questionContainer = document.getElementById("question-container");
+  questionContainer.style.display = "block";
+
+  document.getElementById("question").innerText = question.question;
+  const options = document.getElementById("options");
+  options.innerHTML = "";
+
+  question.options.forEach((option) => {
+    const list = document.createElement("li");
+    list.innerText = option;
+    list.onclick = () => checkAnswer(list, question.answer);
+    options.appendChild(list);
+  });
+}
+
+function checkAnswer(selectedElement, correct) {
+  const options = document.querySelectorAll("#options li");
+
+  options.forEach((option) => {
+    option.style.pointerEvents = "none"; //cannot click options after one is selected
+
+    if (option.innerText === correct) {
+      option.style.backgroundColor = "#03C03C";
+      option.style.color = "white";
+    } else if (option === selectedElement) {
+      option.style.backgroundColor = "#ED1B24";
+      option.style.color = "white";
     }
-  }
-  
+  });
+}
