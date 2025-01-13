023df9ffb8390cc3c77e73b745c1912dd58b0de7
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2025-01-12 21:48:18 -0500

    error handling middleware import no longer needed

--- routes/leaderboardRoutes.js
+++ routes/leaderboardRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
-const error = require("../utilities/error")
 
 // Path to the leaderboard.json file
 const leaderboardFilePath = path.join(__dirname, "../data/leaderboard.json");
